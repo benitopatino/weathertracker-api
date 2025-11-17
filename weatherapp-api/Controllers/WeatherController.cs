557c6cadc2712354c1168420bb570f9473c5bff0
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2025-11-16 17:23:45 -0800

    add basic weather controller

---
+++ weatherapp-api/Controllers/WeatherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace weatherapp_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("temperature/{city}")]
+        public IActionResult GetTemperature(string city)
+        {
+            return Ok("City " + city);
+        }
+
+    }
+}
